052bd77f55563e4b5480c502d3e1e1a93c2b6610
god4k1ta <emir.burakov.78@inbox.ru>
2026-09-23 15:56:03 +0300

    Experiment with OCR detection and recognition

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -1,26 +1,52 @@
-from paddleocr import TextDetection
-model = TextDetection()
-output = model.predict(input="cursor_area.png", batch_size=1)
+from paddleocr import TextDetection, TextRecognition
+from PIL import Image
+
+detection_model = TextDetection()
+recognition_model = TextRecognition()
+
+output = detection_model.predict(
+    input="cursor_area.png",
+    batch_size=1
+)
+
 for res in output:
     dt_polys = res["dt_polys"]
 
-    for polygon in dt_polys:
-        x_array = []
-        y_array = []
+    dt_polys_sorted = sorted(
+        dt_polys,
+        key=lambda poly: min(point[1] for point in poly)
+    )
 
-        for point in polygon:
-            x = point[0]
-            y = point[1]
+    image = Image.open("cursor_area.png")
 
-            x_array.append(x)
-            y_array.append(y)
+    # Берём первый polygon после сортировки
+    polygon = dt_polys_sorted[0]
 
-        left = min(x_array)
-        right = max(x_array)
-        top = min(y_array)
-        bottom = max(y_array)
+    x_array = []
+    y_array = []
 
-        print(left, top, right, bottom)
-    res.print()
-    res.save_to_img(save_path="./output/")
-    res.save_to_json(save_path="./output/res.json")
+    for point in polygon:
+        x = point[0]
+        y = point[1]
+
+        x_array.append(x)
+        y_array.append(y)
+
+    left = min(x_array)
+    right = max(x_array)
+    top = min(y_array)
+    bottom = max(y_array)
+
+    # Вырезаем найденную область
+    cropped = image.crop((left, top, right, bottom))
+    cropped.save("crop_screen.png")
+
+    # Распознаём вырезанную область
+    output2 = recognition_model.predict(
+        input="crop_screen.png",
+        batch_size=1
+    )
+
+    for res2 in output2:
+        rec_text = res2["rec_text"]
+        print(rec_text)
